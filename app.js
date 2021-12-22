2dd809d565d27e48395858b7f54863c567c0a204
Kryspin Kucha <blackcaer@onet.pl>
2021-12-23 00:12:21 +0100

    Setting up basic things & preparing rb_items (done)

--- app.js
+++ app.js
@@ -1,31 +1,58 @@
-//const fs=require('fs')    //file stream
+const fs=require('fs')    //file stream
 //const prompt = require('prompt')
 const rb = require('./lib/sitereq_rb.js')
 const sm = require('./lib/sitereq_sm')
 
+const PATH_RB_ITEMS="./src/testsrc.txt"
+const PATH_SM_ITEMDB="./src/itemdb.txt"
 
-// Main:
-;
+const isNotRedundant = function(item,arr){
+    for(const arr_item of arr)
+    {
+        if (item.name===arr_item.name)
+            return false
+    }
+    return true
+}
+
+;// Main:
 (async()=>{
-    var data=[]
-    var rb_items=[]
 
-while(1)    // fetch rb items
+var data=[]
+var rb_items=[]
+
+{//preparing rb_iems
+
+/*while(1)    // fetch rb items
 {
-    let chunk = [1,2,3,4]
-    //chunk = rb.getItemsChunk()
-    
+    let chunk = []
+    chunk = await rb.getItemsChunk()
+    console.log(chunk)
+
     chunk.forEach(element => {
         if(!(element in rb_items))
             rb_items.push(element)
     })
-    console.log(chunk)
 
-    console.log("XXX: ",rb_items)
+    console.log("XXX: "+rb_items)
     if(1)   // TODO
         break
+}*/
+
+let tmp = JSON.parse(fs.readFileSync(PATH_RB_ITEMS,{encoding:'utf8', flag:'r'}))
+
+for (const item of tmp)
+{
+   // console.log("item in tmp: "+item)
+    if(isNotRedundant(item,rb_items))
+            rb_items.push(item)
 }
-console.log("1")
+}//end preparing rb_iems
+
+//console.log(rb_items)
+console.log(rb_items.length)
+
+let sm_itemsdb = JSON.parse(fs.readFileSync(PATH_SM_ITEMDB,{encoding:'utf8', flag:'r'}))
 
 // pobierz dane ze strony bez kopii 
 //gotowa tablica z itemami (obiekty?)
@@ -39,3 +66,4 @@ console.log("1")
 
 // https://rustbet.com/api/steamInventory   jesli zalogowany
 // https://rustbet.com/api/upgrader/stock?order=1&max=20
+// https://rustbet.com/api/upgrader/stock?order=-1&max=9999&count=28000
